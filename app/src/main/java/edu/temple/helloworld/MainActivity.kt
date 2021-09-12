7c2615d31fce3cd55de6ce6771019d568c4337ac
jason-zhang-github <jasonzhangonline@gmail.com>
2021-09-12 15:58:53 -0400

    Added fixed OCL, not attached to button

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,15 +2,32 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import android.widget.Toast
+// import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<Button>(R.id.button)
-            btn.setOnClickListener(v, -> (v as Button).text = "You clicked the button")
+
+        val click = object : View.OnClickListener
+        {
+            override fun onClick(v: View?)
+            {
+                Toast.makeText(this@MainActivity, "You clicked the button", Toast.LENGTH_LONG).show()
+                Log.d("Message", "You clicked the button. congrats.")
+            }
+
+        }
+        /* btn.setOnClickListener(new View.OnClickListener)
+        {
+            public void onClick(View v)
+        } */
     }
 }
