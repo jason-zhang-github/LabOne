3ba326c1c66db140fae775327d02b6c6ce779987
jason-zhang-github <jasonzhangonline@gmail.com>
2021-09-12 20:05:56 -0400

    Changed lab code to change TextView

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.util.Log
+import android.widget.TextView
+
 // import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
@@ -14,20 +17,22 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.button)
+        val msg = findViewById<TextView>(R.id.textView)
+        val btn = findViewById<Button>(R.id.button) // assign button from xml
+        btn.setOnClickListener{msg.text  = "You clicked the button"}
+        // btn.setOnClickListener({v, -> (v as Button).text = "You clicked the button"})
 
-        val click = object : View.OnClickListener
+        /* val click = object : View.OnClickListener // create OCL
         {
-            override fun onClick(v: View?)
+            override fun onClick(v: View?) //What to do when clicked
             {
                 Toast.makeText(this@MainActivity, "You clicked the button", Toast.LENGTH_LONG).show()
-                Log.d("Message", "You clicked the button. congrats.")
+                Log.d("Message", "You clicked the button")
             }
 
         }
-        /* btn.setOnClickListener(new View.OnClickListener)
-        {
-            public void onClick(View v)
-        } */
+
+        btn.setOnClickListener(click) // assign OCL to button
+        */
     }
 }
